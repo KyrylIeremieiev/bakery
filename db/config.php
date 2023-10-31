46f9f7d85436631b02466fead3a65d2b1eb96741
KyrylIeremieiev <34677@ma-web.nl>
2023-10-31 10:45:54 +0100

    add .env

--- db/config.php
+++ db/config.php
@@ -1,6 +1,10 @@
 <?php
-define('DB_HOST', 'mariadb');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '4dy5qwtrsag#!sad');
-define('DB_NAME', 'voorbeeld_db');
+$envSettings = [];
+if (file_exists(dirname(__FILE__).'/.env')) {
+    $envSettings = parse_ini_file(dirname(__FILE__).'/.env');
+}
 
+define('DB_NAME', (isset($envSettings['DB_NAME'])) ? $envSettings['DB_NAME'] : 'example');
+define('DB_USERNAME', (isset($envSettings['DB_USER'])) ? $envSettings['DB_USERNAME'] : 'root');
+define('DB_PASSWORD', (isset($envSettings['DB_PASSWORD'])) ? $envSettings['DB_PASSWORD'] : 'pass');
+define('DB_HOST', (isset($envSettings['DB_HOST'])) ? $envSettings['DB_HOST'] : 'localhost');
